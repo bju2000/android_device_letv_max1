d3094511c290fe88b5a37a9c1ea1f9d91a1a1776
Alexandre Heitor Schmidt <alexandre.schmidt@gmail.com>
2019-01-21 18:44:08 +0000

    check_proprietary_files.sh now can look into a local directory as well as via adb. proprietary-files.txt corrected to have the NOT FOUND lines commented.

--- check_proprietary_files.sh
+++ check_proprietary_files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+use_adb=1
+if [ "$1" != "" ]
+then
+    echo "Using directory '$1' to check."
+    use_adb=0
+    if [ ! -d "$1" ]
+    then
+        echo "Input directory '$1' not found."
+        exit 1
+    fi
+fi
+
 echo "Saving to proprietary-files.txt.new"
 
 > proprietary-files.txt.new
@@ -14,10 +26,26 @@ do
         then
             thefile=$(echo "$thefile" | cut -d '-' -f 2-)
         fi
-        exists=$(adb shell -x -n "if [ -e '/system/$thefile' ]; then echo -n 1; else echo -n 0; fi")
-        if [ $? -ne 0 ]
+
+        exists=0
+        if [ "$use_adb" -eq 1 ]
         then
-            echo "error in adb"
+            exists=$(adb shell -x -n "if [ -e '/system/$thefile' ]; then echo -n 1; else echo -n 0; fi")
+            if [ $? -ne 0 ]
+            then
+                echo "error in adb"
+                exit 1
+            fi
+        elif [ "$use_adb" -eq 0 ]
+        then
+            if [ -e "$1/system/$thefile" ]
+            then
+                exists=1
+            else
+                exists=0
+            fi
+        else
+            echo "Unknown value for use_adb: $use_adb"
             exit 1
         fi
 
