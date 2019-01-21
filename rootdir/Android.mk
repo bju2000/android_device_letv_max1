2148aedbb734998d7a5dbb04e56c95fad7e69512
Alexandre Heitor Schmidt <alexandre.schmidt@gmail.com>
2019-01-21 17:56:33 +0000

    Removed specific oneplus2 stuff and renamed other stuff to max1.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,10 +35,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.recovery.oneplus2.rc
+LOCAL_MODULE		:= init.recovery.max1.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.recovery.oneplus2.rc
+LOCAL_SRC_FILES		:= etc/init.recovery.max1.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -52,10 +52,10 @@ include $(BUILD_PREBUILT)
 
 # Configuration scripts
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.oneplus2.power.sh
+LOCAL_MODULE		:= init.max1.power.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= EXECUTABLES
-LOCAL_SRC_FILES		:= etc/init.oneplus2.power.sh
+LOCAL_SRC_FILES		:= etc/init.max1.power.sh
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
